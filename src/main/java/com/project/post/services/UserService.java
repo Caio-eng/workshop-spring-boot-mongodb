5aa83a757b194ec41abb4df7f8122bd9cd92ec7b
Caio Cesar <caiopereira1998@hotmail.com>
2021-12-24 18:53:51 -0300

    Obtendo um usuário por id

--- src/main/java/com/project/post/services/UserService.java
+++ src/main/java/com/project/post/services/UserService.java
@@ -1,12 +1,14 @@
 package com.project.post.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.post.domain.User;
 import com.project.post.repository.UserRepository;
+import com.project.post.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
 	}
 	
 }
