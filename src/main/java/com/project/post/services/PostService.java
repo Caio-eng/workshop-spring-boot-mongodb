7af3a78746a779fd40141c3bec99952a90a95f43
Caio Cesar <caiopereira1998@hotmail.com>
2021-12-31 15:55:52 -0300

    CRUD Post

---
+++ src/main/java/com/project/post/services/PostService.java
@@ -0,0 +1,55 @@
+package com.project.post.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.post.domain.Post;
+import com.project.post.dto.PostDTO;
+import com.project.post.repository.PostRepository;
+import com.project.post.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
+	}
+	
+	public Post insert(Post obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public Post update(Post obj) {
+		Post newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Post newObj, Post obj) {
+		newObj.setAuthor(obj.getAuthor());
+		newObj.setBody(obj.getBody());
+		newObj.setTitle(obj.getTitle());
+		newObj.setDate(obj.getDate());
+	}
+
+	public Post fromDTO(PostDTO objDTO) {
+		return new Post(objDTO.getId(), objDTO.getDate(), objDTO.getTitle(), objDTO.getBody(), objDTO.getAuthor());
+	}
+	
+}
