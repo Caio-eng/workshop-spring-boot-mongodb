a1755066c13e45861b1ac2893c54c8e05ff8bd75
Caio Cesar <caiopereira1998@hotmail.com>
2022-01-01 14:40:58 -0300

    Consulta simples com @Query

--- src/main/java/com/project/post/repository/PostRepository.java
+++ src/main/java/com/project/post/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.project.post.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.post.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String title);
 
 	List<Post> findByTitleContainingIgnoreCase(String title);
 	
--- src/main/java/com/project/post/services/PostService.java
+++ src/main/java/com/project/post/services/PostService.java
@@ -42,7 +42,8 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.findByTitle(text); 
+	  //repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 	private void updateData(Post newObj, Post obj) {
