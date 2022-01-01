78cf4af98cf5ba6ac3730299e2607ac99a8eebef
Caio Cesar <caiopereira1998@hotmail.com>
2022-01-01 15:31:07 -0300

    Consulta com vários critérios

--- src/main/java/com/project/post/config/Instantiation.java
+++ src/main/java/com/project/post/config/Instantiation.java
@@ -41,7 +41,7 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("01/01/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		ComentDTO c1 = new ComentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		ComentDTO c2 = new ComentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
